b01b52fde90223908d2f3c9abb98cf2aa5cceca3
Stan Lemon <stanlemon@users.noreply.github.com>
2019-02-07 20:51:08 -0500

    A vanilla HOC example to tinker with

--- apps/react-pouchdb-example/src/App.tsx
+++ apps/react-pouchdb-example/src/App.tsx
@@ -3,16 +3,19 @@ import { Container, withDocument } from "./PouchDb";
 import Counter from "./Counter";
 import { Notes } from "./Notes";
 import "./App.css";
+import { withContainer, Header } from "./withContainer";
 
 const WrappedCounter = withDocument("counter", Counter);
 const WrappedNotes = withDocument("notes", Notes);
+
+const WrappedHeader = withContainer(Header);
 
 export default class App extends React.Component {
   render(): React.ReactNode {
     return (
       <Container database="local">
         <div className="app">
-          <h1>Counter App!</h1>
+          <WrappedHeader>Counter App!</WrappedHeader>
           <WrappedCounter count={0} loading={<div>Loading...</div>} />
           <WrappedNotes loading={<div>Loading...</div>} />
         </div>
---
+++ apps/react-pouchdb-example/src/withContainer.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+
+export interface ContainerProps {
+  loading?: string;
+}
+
+/* eslint-disable max-lines-per-function */
+export function withContainer<OriginalProps extends {}>(
+  WrappedComponent: React.ComponentType<OriginalProps>
+): React.ComponentType<OriginalProps & ContainerProps> {
+  type CombinedProps = OriginalProps & ContainerProps;
+
+  class Container extends React.Component<CombinedProps> {
+    /* eslint-disable @typescript-eslint/no-object-literal-type-assertion */
+    static defaultProps: CombinedProps = {
+      loading: "Loading!!!"
+    } as CombinedProps;
+
+    state = {
+      initialized: false
+    };
+
+    componentDidMount(): void {
+      // Simulate remote data load, 2 minutes after we mounted set initialized to true
+      setTimeout(() => {
+        this.setState({
+          initialized: true
+        });
+      }, 500);
+    }
+
+    render(): React.ReactNode {
+      if (!this.state.initialized) {
+        return <div>{this.props.loading}</div>;
+      }
+
+      const props = {
+        ...this.props
+      };
+
+      return <WrappedComponent {...props} />;
+    }
+  }
+  return Container;
+}
+
+export interface HeaderProps {
+  size?: number;
+  children: React.ReactChildren | React.ReactText;
+}
+
+export class Header extends React.Component<HeaderProps> {
+  static defaultProps: Partial<HeaderProps> = {
+    size: 1
+  };
+
+  render(): React.ReactNode {
+    const H = (
+      props: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLHeadingElement>,
+        HTMLHeadingElement
+      >
+    ): JSX.Element => React.createElement("h" + this.props.size, props);
+
+    return (
+      <div>
+        <H>{this.props.children}</H>
+      </div>
+    );
+  }
+}
+
+export const WrappedHeader = withContainer(Header);
+
+export const WrappedHeaderExample = <WrappedHeader>Hello World</WrappedHeader>;
