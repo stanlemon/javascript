5bcd742c287b84280d826c9d836d4594c955b4b4
Stan Lemon <stanlemon@users.noreply.github.com>
2020-07-07 19:18:24 -0400

    Updates and tests

---
+++ apps/react-pouchdb-example/src/App.test.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { render, waitFor, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PouchDB from "pouchdb";
+import { App } from "./App";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+PouchDB.plugin(require("pouchdb-adapter-memory"));
+
+jest.useFakeTimers();
+
+describe("<App />", () => {
+  afterEach(cleanup);
+
+  // eslint-disable-next-line jest/expect-expect
+  it("renders", async () => {
+    const db = new PouchDB("test", {
+      adapter: "memory",
+    });
+
+    const { getByText } = render(<App db={db} />);
+
+    await waitFor(() => getByText("Notes:"));
+    await waitFor(() => getByText("Tasks:"));
+  });
+});
--- apps/react-pouchdb-example/src/DocumentHelpers.test.ts
+++ apps/react-pouchdb-example/src/DocumentHelpers.test.ts
@@ -1,5 +1,76 @@
-import { addRow } from "./DocumentHelpers";
+import {
+  addRow,
+  removeRow,
+  removeRowById,
+  updateRow,
+  updatePartialRow,
+} from "./DocumentHelpers";
 
 test("addRows()", () => {
-  expect(addRow([], { foo: "bar" })).toMatchObject([{ foo: "bar" }]);
+  // No rows yet
+  expect(addRow([], { foo: "foo" })).toMatchObject([{ foo: "foo" }]);
+
+  // A new row should get an id field
+  expect(addRow([], { foo: "foo" })[0].id).toBeDefined();
+
+  // Existing rows
+  expect(addRow([{ id: "1", bar: "bar" }], { foo: "foo" })).toMatchObject([
+    { id: "1", bar: "bar" },
+    { foo: "foo" },
+  ]);
+});
+
+test("removeRow()", () => {
+  expect(
+    removeRow(
+      [
+        { id: "1", foo: "foo" },
+        { id: "2", bar: "bar" },
+      ],
+      { id: "2" }
+    )
+  ).toEqual([{ id: "1", foo: "foo" }]);
+});
+
+test("removeRowById()", () => {
+  expect(
+    removeRowById(
+      [
+        { id: "1", foo: "foo" },
+        { id: "2", bar: "bar" },
+      ],
+      "2"
+    )
+  ).toEqual([{ id: "1", foo: "foo" }]);
+});
+
+test("updateRow()", () => {
+  expect(
+    updateRow(
+      [
+        { id: "1", foo: "foo" },
+        { id: "2", bar: "bar" },
+      ],
+      { id: "2", bar: "baz" }
+    )
+  ).toEqual([
+    { id: "1", foo: "foo" },
+    { id: "2", bar: "baz" },
+  ]);
+});
+
+test("updatePartialRow()", () => {
+  expect(
+    updatePartialRow(
+      [
+        { id: "1", foo: "foo" },
+        { id: "2", bar: "bar" },
+      ],
+      "2",
+      { baz: "baz" }
+    )
+  ).toEqual([
+    { id: "1", foo: "foo" },
+    { id: "2", bar: "bar", baz: "baz" },
+  ]);
 });
