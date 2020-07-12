54c7ee9368e87e2751c782db5f5f6cf1be562049
Stan Lemon <stanlemon@users.noreply.github.com>
2020-07-12 13:20:36 -0400

    Support not logging in

--- apps/react-pouchdb-example/src/App.tsx
+++ apps/react-pouchdb-example/src/App.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
 import { Database, Document } from "@stanlemon/react-pouchdb";
-import { Login as LoginView } from "./Login";
-import { SignUp as SignUpView } from "./SignUp";
-import { Notes } from "./Notes";
-import { Tasks } from "./Tasks";
 import {
   Authentication,
+  Context as AuthenticationContext,
+  Authenticated,
+  Unauthenticated,
   withAuthentication,
   Login,
   SignUp,
 } from "@stanlemon/react-couchdb-authentication";
-import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bulma/css/bulma.css";
+import "./App.css";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Login as LoginView } from "./Login";
+import { SignUp as SignUpView } from "./SignUp";
+import { Notes } from "./Notes";
+import { Tasks } from "./Tasks";
 
 const remoteUrl = process.env.REMOTE_URL
   ? process.env.REMOTE_URL
@@ -42,51 +45,63 @@ export const App = withAuthentication(
     db: PouchDB.Database;
     remoteDb: PouchDB.Database;
     user?: { name: string; email: string };
-  }): React.ReactElement => (
-    <div>
-      <Header title="Test App" subtitle="Notes, tasks and stuff like that" />
+  }): React.ReactElement => {
+    return (
+      <div>
+        <Header title="Test App" subtitle="Notes, tasks and stuff like that" />
 
-      <div className="app container">
-        <div className="columns">
-          <div className="column">
-            {user && user.name && (
-              <h1>
-                Hello, <a href={`mailto:${user.email}`}>{user.name}</a>!
-              </h1>
-            )}
-          </div>
-          <div className="column has-text-right">
-            <button className="button is-small" onClick={logout}>
-              <span className="icon is-small">
-                <i className="fas fa-sign-out-alt" />
-              </span>
-              <span>Logout</span>
-            </button>
+        <div className="app container">
+          <Authenticated>
+            <div className="columns is-mobile is-gapless is-vcentered">
+              <div className="column is-half">
+                {user && user.name && (
+                  <h1>
+                    Hello, <a href={`mailto:${user.email}`}>{user.name}</a>!
+                  </h1>
+                )}
+              </div>
+              <div className="column  is-half has-text-right">
+                <button className="button is-small" onClick={logout}>
+                  <span className="icon is-small">
+                    <i className="fas fa-sign-out-alt" />
+                  </span>
+                  <span>Logout</span>
+                </button>
+              </div>
+            </div>
+          </Authenticated>
+          <Unauthenticated>
+            <AuthenticationContext.Consumer>
+              {({ login, error }) => (
+                <Login error={error} login={login} component={LoginView} />
+              )}
+            </AuthenticationContext.Consumer>
+          </Unauthenticated>
+
+          <hr />
+
+          <div className="columns">
+            <Database debug={true} database={db} remote={remoteDb}>
+              <div className="column">
+                <WrappedNotes />
+              </div>
+              <div className="column">
+                <WrappedTasks />
+              </div>
+            </Database>
           </div>
         </div>
-
-        <hr />
-
-        <div className="columns">
-          <Database debug={true} database={db} remote={remoteDb}>
-            <div className="column">
-              <WrappedNotes />
-            </div>
-            <div className="column">
-              <WrappedTasks />
-            </div>
-          </Database>
-        </div>
+        <br />
+        <Footer />
       </div>
-      <br />
-      <Footer />
-    </div>
-  )
+    );
+  }
 );
 
 export default function (): React.ReactElement {
   return (
     <Authentication
+      scaffold={false}
       debug={true}
       url={remoteUrl}
       // Disable sync because the <Database/> component will manage this for us
--- apps/react-pouchdb-example/src/Login.tsx
+++ apps/react-pouchdb-example/src/Login.tsx
@@ -1,65 +1,63 @@
 import * as React from "react";
-import { Header } from "./Header";
 import { LoginViewProps as Props } from "@stanlemon/react-couchdb-authentication/dist/components/LoginView";
 
 export function Login(props: Props): React.ReactElement {
   return (
-    <div>
-      <Header title="Test App" subtitle="Login to get started" />
-
-      <br />
-
-      <div className="container">
-        <div className="columns">
-          <div className="column is-three-fifths is-offset-one-fifth">
-            {props.error && (
-              <div className="notification is-danger">{props.error}</div>
-            )}
-
-            <div className="field">
-              <label className="label">Username</label>
-              <div className="control has-icons-left has-icons-right">
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Username"
-                  value={props.username}
-                  onChange={props.setUsername}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-user" />
-                </span>
-              </div>
+    <>
+      {props.error && (
+        <div className="notification is-danger">{props.error}</div>
+      )}
+      <div className="field is-horizontal">
+        <div className="field-body">
+          <div className="field">
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="text"
+                placeholder="Username"
+                value={props.username}
+                onChange={props.setUsername}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-user" />
+              </span>
             </div>
-            <div className="field">
-              <label className="label">Password</label>
-              <p className="control has-icons-left">
-                <input
-                  className="input"
-                  type="password"
-                  placeholder="Password"
-                  value={props.password}
-                  onChange={props.setPassword}
-                  onKeyDown={(e): void => {
-                    // Submit the form if they hit enter
-                    if (e.key.toLowerCase() === "enter") {
-                      props.login();
-                    }
-                  }}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fas fa-lock" />
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <p className="control">
-                <button className="button is-success" onClick={props.login}>
-                  Login
-                </button>
-              </p>
-            </div>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                value={props.password}
+                onChange={props.setPassword}
+                onKeyDown={(e): void => {
+                  // Submit the form if they hit enter
+                  if (e.key.toLowerCase() === "enter") {
+                    props.login();
+                  }
+                }}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock" />
+              </span>
+            </p>
+          </div>
+          <button className="button is-success" onClick={props.login}>
+            Login
+          </button>
+        </div>
+      </div>
+      <p className="has-text-centered">
+        <button className="button is-text" onClick={props.navigateToSignUp}>
+          Don't have an account? Click here to sign up
+        </button>
+      </p>
+    </>
+  );
+}
 
+/*
             <p>
               <button
                 className="button is-text"
@@ -68,9 +66,4 @@ export function Login(props: Props): React.ReactElement {
                 Click here to sign up
               </button>
             </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+*/
