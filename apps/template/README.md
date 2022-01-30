348261d7c0e35b027227b703695d079fedfb50c5
Stan Lemon <stanlemon@users.noreply.github.com>
2022-01-30 16:44:58 -0500

    Improvements to cli and READMEs

---
+++ apps/template/README.md
@@ -0,0 +1,3 @@
+# App
+
+A web application built with [React](http://reactjs.org) using [@stanlemon/webdev](https://www.npmjs.com/package/@stanlemon/webdev).
--- packages/cli/index.js
+++ packages/cli/index.js
@@ -4,7 +4,10 @@ import yargs from "yargs";
 import process from "process";
 import path from "path";
 import fs from "fs";
-import { getInstalledPath } from "get-installed-path";
+import { getInstalledPathSync } from "get-installed-path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // eslint-disable-next-line no-unused-expressions
 yargs(process.argv.slice(2))
@@ -22,28 +25,36 @@ yargs(process.argv.slice(2))
         .demandOption("name");
     },
     ({ name }) => {
-      getInstalledPath("@stanlemon/app-template").then((src) => {
-        const dest = path.join(process.cwd(), name);
+      let src;
+      try {
+        // For standard setups where you install the CLI
+        src = getInstalledPathSync("@stanlemon/app-template", {
+          local: true,
+        });
+      } catch (err) {
+        // Most likely for npx use cases (like in the README)
+        src = path.resolve(__dirname, "./../app-template/");
+      }
+      const dest = path.join(process.cwd(), name);
 
-        console.log(`Creating app:`);
-        console.log(`  from = ${src}`);
-        console.log(`  to = ${dest}`);
-        console.log(``);
+      console.log(`Creating app:`);
+      console.log(`  from = ${src}`);
+      console.log(`  to = ${dest}`);
+      console.log(``);
 
-        fs.cp(
-          src,
-          dest,
-          {
-            dereference: true,
-            force: true,
-            preserveTimestamps: true,
-            recursive: true,
-          },
-          (e) => {
-            console.log("All done!");
-          }
-        );
-      });
+      fs.cp(
+        src,
+        dest,
+        {
+          dereference: true,
+          force: true,
+          preserveTimestamps: true,
+          recursive: true,
+        },
+        (e) => {
+          console.log("All done!");
+        }
+      );
     }
   )
   .help().argv;
