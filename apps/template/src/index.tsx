04c0798bc954890b0cb89ee3d839ce6b65c50797
Stan Lemon <stanlemon@users.noreply.github.com>
2022-04-12 19:38:08 -0400

    Upgrade React render, minor tweaks

--- apps/template/src/index.tsx
+++ apps/template/src/index.tsx
@@ -1,4 +1,17 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(
+  document.body.appendChild(document.createElement("div"))
+);
+root.render(<App />);
+
+const link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute(
+  "href",
+  "https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css"
+);
+
+document.getElementsByTagName("head")[0].appendChild(link);
