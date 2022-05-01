85bc3636876c685cfe0a860e3cee7fb37d64168f
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2022-05-01 12:48:32 -0400

    Monkey patch setImmediate

--- packages/webdev/jest.setup.js
+++ packages/webdev/jest.setup.js
@@ -3,3 +3,8 @@ require("@testing-library/jest-dom");
 const { Crypto } = require("@peculiar/webcrypto");
 
 global.crypto = new Crypto();
+
+if (!window.setImmediate) {
+  // This is as gross as it looks. It's a workaround for using PouchDB in tests.
+  window.setImmediate = window.setTimeout; // ts: as unknown as typeof setImmediate;
+}
