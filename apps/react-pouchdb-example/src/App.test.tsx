4b2d5984e62bb7a55f6b81bd4d3e22cf843088d6
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2022-05-08 13:20:40 -0400

    Fix tsc bug

--- apps/react-pouchdb-example/src/App.test.tsx
+++ apps/react-pouchdb-example/src/App.test.tsx
@@ -1,24 +1,17 @@
 import * as React from "react";
-import { render, act, screen, waitFor, cleanup } from "@testing-library/react";
+import { render, act, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import PouchDB from "pouchdb";
 import { App } from "./App";
 // https://github.com/facebook/jest/issues/11511#issuecomment-853355787
 import { setImmediate } from "timers";
+import PouchDBMemoryAdapter from "pouchdb-adapter-memory";
+
+PouchDB.plugin(PouchDBMemoryAdapter);
 
 window.setImmediate = setImmediate;
 
 describe("<App />", () => {
-  beforeAll(async () => {
-    const { default: PouchDBMemoryAdapter } = await import(
-      "pouchdb-adapter-memory"
-    );
-
-    PouchDB.plugin(PouchDBMemoryAdapter);
-  });
-
-  afterEach(cleanup);
-
   // eslint-disable-next-line jest/expect-expect
   it("renders", async () => {
     const db = new PouchDB("test", {
