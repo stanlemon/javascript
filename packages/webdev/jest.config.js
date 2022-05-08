4e4d3e6f4d0795b0c6b20964b100589d3f8f6514
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2022-05-08 12:48:53 -0400

    Switch back to commonjs for web packages

--- packages/webdev/jest.config.js
+++ packages/webdev/jest.config.js
@@ -1,17 +1,11 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import { readFileSync } from "fs";
+const path = require("path");
 
-const babelOptions = JSON.parse(
-  readFileSync(new URL("./.babelrc.json", import.meta.url))
-);
+const babelOptions = require("./.babelrc.json");
 
 // Disable using esmodules everywhere
 babelOptions.presets.find(
   (preset) => preset[0] === "@babel/preset-env"
 )[1].modules = "auto";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // These are packages we know ship with esmodules and need to be transformed
 const esModules = [
@@ -25,7 +19,7 @@ const esModules = [
   "@stanlemon/react-couchdb-authentication",
 ].join("|");
 
-export default {
+module.exports = {
   verbose: true,
   setupFilesAfterEnv: [path.resolve(__dirname, "./jest.setup.js")],
   testEnvironment: "jsdom",
