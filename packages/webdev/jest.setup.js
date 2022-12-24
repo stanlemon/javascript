d611c6fb126c37700d4dfd51d543ad25b7fb9362
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2022-12-24 17:04:08 -0500

    Appear this may not be required anymore

--- packages/webdev/jest.setup.js
+++ packages/webdev/jest.setup.js
@@ -1,9 +1,5 @@
 require("@testing-library/jest-dom");
 
-const { Crypto } = require("@peculiar/webcrypto");
-
-global.crypto = new Crypto();
-
 if (!global.setImmediate) {
   // This is as gross as it looks. It's a workaround for using PouchDB in tests.
   global.setImmediate = global.setTimeout; // ts: as unknown as typeof setImmediate;
