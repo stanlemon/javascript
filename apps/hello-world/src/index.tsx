10fae3b428be7ad35a3d21597d4b1b897a4845f1
Stan Lemon <stanlemon@users.noreply.github.com>
2024-02-18 12:47:58 -0500

    Add simple hello world example of webdev

---
+++ apps/hello-world/src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import Home from "./components/Home";
+
+const root = createRoot(
+  document.body.appendChild(document.createElement("div"))
+);
+root.render(<Home />);
